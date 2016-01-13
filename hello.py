32a723c27feb536e5cc40fdf01e5236642ca488d
mjonyh <mjonyh@gmail.com>
2016-01-13 19:55:15 +0600

    script added

---
+++ hello.py
@@ -0,0 +1,10 @@
+'''
+This is the hello world script in Python.
+The first program written in Python.
+'''
+
+'''
+print command will print the string
+'Hello World!'
+'''
+print 'Hello World!'
---
+++ sample_example_1.py
@@ -0,0 +1,14 @@
+'''
+This is the sample example script!
+'''
+
+
+'''
+Predefined argument
+'''
+x = 1
+
+'''
+Print the argument
+'''
+print x
